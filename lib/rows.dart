83ce5478856e59a22c0620d022ffb7fee41bde63
sumangoyal1021 <sumangoyal1021@gmail.com>
2020-10-21 09:29:35 +0530

    Using flutter and dart

---
+++ lib/rows.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Home(),
+));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text('Rows'),
+          centerTitle: true,
+          backgroundColor: Colors.teal[900]
+      ),
+      body: Container(
+       child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Text('Suman Goyal'),
+          RaisedButton(
+            onPressed: () {},
+            color: Colors.amber,
+            child: Text('click me'),
+          ),
+          Container(
+              color: Colors.green,
+              padding: EdgeInsets.all(30.0),
+              child: Text('inside container')
+          ),
+          SizedBox(
+            height: 100.0,
+          ),
+          Row(
+              children: <Widget>[
+                Container(
+                    color: Colors.green,
+                    child: Text('inside container'),
+                    height: 80.0,
+                    width: 70.0,
+                ),
+                SizedBox(
+                  width: 20.0,
+                ),
+                Container(
+                  color: Colors.green,
+                  child: Text('inside container'),
+                  height: 80.0,
+                  width: 70.0,
+                ),
+              ]
+          )
+        ],
+      ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('suman');
+        },
+        backgroundColor: Colors.red[600],
+        child: Text('click'),
+      ),
+    );
+  }
+}
